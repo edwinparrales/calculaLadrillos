11bf81d761daf69f5dc774e53d09c183d49f5ce2
eparrales <edwinparrales82@gmail.com>
2022-08-09 11:51:15 -0500

    Add files via upload

---
+++ js/Ladrillo.js
@@ -0,0 +1,18 @@
+export default class Ladrillo{
+
+    constructor(alto,largo,grosor){
+       this.alto=alto;
+       this.largo=largo;
+       this.grosor=grosor;
+    }
+
+    obtenerAlto(){
+        return this.alto/100;
+    }
+    obtenerLargo(){
+        return this.largo/100;
+    }
+    obtenerGrosor(){
+        return alto/100;
+    }
+}
--- js/main.js
+++ js/main.js
@@ -1,7 +1,28 @@
-const result = document.querySelector("#resultado");
-const val1 = document.querySelector("#val1");
-const val2 = document.querySelector("#val2");
-console.log(val1.value);
-function sumar(event) {
-    result.innerHTML=parseInt(val1.value)+parseInt(val2.value);
+import Ladrillo from "./ladrillo.js";
+
+const btnCalcular = document.getElementById("btn-calcular");
+let result  = document.getElementById("respuesta")
+let calCantLadrillos = ()=>{
+    let altura = document.getElementById("altura");
+    let junVer = document.getElementById("junVertical");
+    let largo = document.getElementById("largo");
+    let junHor = document.getElementById("junHorizontal");
+
+    
+
+    let ladrillo = new Ladrillo(altura.value,largo.value,10);
+  
+   
+    let alturaTotal = ladrillo.obtenerAlto()+(junVer.value/100);
+    let largoTotal = ladrillo.obtenerLargo()+(junHor.value/100);
+    const divisor = alturaTotal*largoTotal;
+    let cant = 1/divisor;
+    cant = Math.ceil(cant);
+    console.log("can",cant);
+    result.innerHTML=`<h2>${cant}</h2>`
+  
 }
+
+btnCalcular.addEventListener('click',calCantLadrillos);
+
+
